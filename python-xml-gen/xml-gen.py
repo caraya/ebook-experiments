3dd780e4c8024b1d85a31c72558793e4a83f5fe5
Carlos Araya <carlos.araya@gmail.com>
2014-09-27 17:28:19 -0700

    continue working in xml-gen script

--- python-xml-gen/xml-gen.py
+++ python-xml-gen/xml-gen.py
@@ -2,19 +2,31 @@
 import mimetypes
 import glob
 import os
+import os.path
 
+# Initialize the mimetypes database
 mimetypes.init()
+# Create the package.opf file
 package = open('package.opf', 'w')
 
-# The filenames of the HTML are listed in html_files
-# This is really clunky and will require edits later but it's 
-# the best I can do right now. 
-package_content = glob.glob('OEBPS/*')
-package_content += glob.glob('OEBPS/*/*')
-package_content += glob.glob('OEBPS/*/*/*')
+# The glob below should encompass everything under 
+# OEBPS. Right now I'm trying to remove empty directories
+# and the package_content variable. 
+# WARNING: This glong will add all files and directories 
+# to the variable. You will have to edit the file and remove
+# empty directories and the package.opf file reference from
+# both the manifest and the spine
+package_content = glob.glob('OEBPS/**/*')
 
+# FIRST PASS AT WRITING FUNCTION TO ADDRESS ISSUE ABOVE
+#for file in os.listdir( location ):
+#  if os.path.isfile(os.path.join('OEBPS', file)):
+#    package_content = ''
+#    package_contet += file
+  
 
-
+# Rather than use a templating system we build the XML portion
+# by hand
 template_top = '''<package xmlns="http://www.idpf.org/2007/opf"
   unique-identifier="book-id"
   version="3.0" xml:lang="en">
@@ -41,15 +53,16 @@ template_bottom = '''</spine>
 manifest = ""
 spine = ""
 
-# Write each HTML file to the ebook, collect information for the index
+# Write the content of OEBPS into the manifest and spines
 for i, item in enumerate(package_content):
     basename = os.path.basename(item)
     mime = mimetypes.guess_type(item, strict=True)
-    manifest += '\t<item id="file_%s" href="%s" media-type="%s"/>\n' % (i+1, basename, mime)
-    spine += '\t<itemref idref="file_%s" />\n' % (i+1)
+    manifest += '\t<item id="file_%s" href="%s" media-type="%s"/>\n' % (i+1, basename, mime[0])
+    spine += '\n\t<itemref idref="file_%s" />' % (i+1)
 
-# I don't remember my python all that well to remember how to print the interpolated
-# content. This should do for now.
+# I don't remember my python all that well to remember 
+# how to print the interpolated content. 
+# This should do for now.
 package.write(template_top)
 package.write(manifest)
 package.write(template_transition)
